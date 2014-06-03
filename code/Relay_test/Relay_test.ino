98b51b8034cadf8267c4085c93f603b56c308994
David Mcanulty <github@hellspark.com>
2014-06-03 14:15:15 -0700

    Relay test program, ironicly untested as of Jun 3rd 2014

---
+++ code/Relay_test/Relay_test.ino
@@ -0,0 +1,27 @@
+/*
+  Relay-test - David Mcanulty - 2014
+  
+  Turns led and relay on and off repeatedly for testing.
+  Pin  6 is the relay control pin on the Bench Buddee Arduino shield
+  Pin 13 has an LED connected on most Arduino boards.
+*/
+
+int msdelay = 5000;
+int led = 13;
+int relay = 6;
+
+// the setup routine runs once when you press reset:
+void setup() {
+  pinMode(led, OUTPUT);     // initialize the digital pin as output.
+  pinMode(relay, OUTPUT);   // initialize the digital pin as output.
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(relay, HIGH); // turn the relay on
+  delay(msdelay);               // wait for a second
+  digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
+  digitalWrite(relay, LOW);    // turn the relay off by making the voltage LOW
+  delay(msdelay);               // wait for a second
+}
